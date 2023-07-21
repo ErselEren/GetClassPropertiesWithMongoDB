453fca5d2569877abbffae055ecf93c5dfc28b03
ErselEren <erseleren_@hotmail.com>
2023-07-21 14:46:21 +0300

    .

--- Program.cs
+++ Program.cs
@@ -11,40 +11,8 @@ namespace mongo1
     {
         public static void Main(string[] args)
         {
-            //func1();
             func2();
         }   
-
-
-        private static void func1()
-        {
-            Author author = new Author { FirstName = "John", LastName = "Doe" };
-            Author author2 = new Author { Id = 2, FirstName = "Jane", LastName = "Doe" };
-            Author author3 = new Author { Id = 3, FirstName = "Michael", LastName = "Smith" };
-            Author author4 = new Author { Id = 4, FirstName = "Kate", LastName = "Mell" };
-
-            // create a client connection
-            var client = new MongoClient("mongodb://localhost:27017");
-
-            // create a database
-            IMongoDatabase db = client.GetDatabase("test");
-
-            // create a collection
-            IMongoCollection<BsonDocument> collection2 = db.GetCollection<BsonDocument>("authors");
-
-            //InsertDocument(collection, author);
-
-            //ListDocuments(collection2);
-
-            Console.WriteLine("-----------------------------------------");
-
-            //DeleteDocumentWithId(collection2, 1);
-
-            List<String> fieldList = GetFields(collection2, "FirstName");
-
-
-            Console.ReadLine();
-        }
 
         private static void func2() 
         {
@@ -57,12 +25,6 @@ namespace mongo1
             //InsertClassNames(collection);
 
             List<ClassNames> list = GetClassNames(collection);
-
-            //print list
-            foreach (ClassNames classNames in list)
-            {
-                //Console.WriteLine(classNames.ClassName + " " + classNames.Namespace);
-            }
 
             //create new collection
             IMongoCollection<ClassNames> collectionWithProperties = database.GetCollection<ClassNames>("ClassNamesWithProperties");
@@ -125,15 +87,6 @@ namespace mongo1
                 //Console.WriteLine("=="+classNames.ClassName + " " + classNames.Namespace);
             }
 
-            //var filter = Builders<ClassNames>.Filter.Empty;
-            //List<ClassNames> list2 = collection.Find(filter).ToList();
-
-            ////print list
-            //foreach (ClassNames classNames in list2)
-            //{
-            //    Console.WriteLine("++" + classNames.ClassName + " " + classNames.Namespace);
-            //}
-
             return classNamesList;
         }
 
@@ -161,50 +114,6 @@ namespace mongo1
                 collection.InsertOne(classNames);
             }
 
-
-
-            //BsonDocument className1 = new BsonDocument { { "ClassName", "mongo1.Student" } };
-            //BsonDocument className2 = new BsonDocument { { "ClassName", "mongo1.School" } };
-            //BsonDocument className3 = new BsonDocument { { "ClassName", "mongo1.Garage" } };
-            //BsonDocument className4 = new BsonDocument { { "ClassName", "mongo1.Author" } };
-            //BsonDocument className5 = new BsonDocument { { "ClassName", "mongo1.City" } };
-            //BsonDocument className6 = new BsonDocument { { "ClassName", "mongo1.Car" } };
-
-            //List<BsonDocument> classNames = new List<BsonDocument>();
-            //classNames.Add(className1);
-            //classNames.Add(className2);
-            //classNames.Add(className3);
-            //classNames.Add(className4);
-            //classNames.Add(className5);
-            //classNames.Add(className6);
-
-            //foreach (BsonDocument className in classNames)
-            //{
-            //    Console.WriteLine(className.GetElement("ClassName").Value);
-            //    Type type = Type.GetType(className.GetElement("ClassName").Value.ToString());
-            //    if (type == null)
-            //    {
-            //        Console.WriteLine("Class : -" + className + "- not found");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Class : -" + className + "- found");
-            //    }
-
-            //}
-
-            //Type type1 = Type.GetType("asdfasdfd");
-            //if(type1 == null)
-            //{
-            //    Console.WriteLine("Class : -" + "RandomClass" + "- not found");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Class : -" + "RandomClass" + "- found");
-            //}
-
-
-            //Console.WriteLine("End of InsertClassNames()");
         }
 
         private static void ListCollections(IMongoDatabase database)
